a45884324a9f16122529c88939caabd313a113aa
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2014-10-23 21:22:39 +0000

    fixed puts bug...found in the attr line

--- marsrover.rb
+++ marsrover.rb
@@ -1,67 +1,68 @@
 class Rover
-	attr_accessor = :x,:y,:direction
+	attr_accessor :x,:y,:direction
 
-def initialize (x,y,direction)
-	@x=x
-	@y=y
-	@direction=direction
-
-end
-def read_instruction (instruction)
-	if instruction == "M"
-		move
-	elsif instruction == "L"
-		turn_left
-	elsif instruction == "R"
-		turn_right
-	else
-		puts "Invalid input"
-
-	end
-end
-def turn_left
-
-if @direction == "N"
-	@direction = "W"
-elsif @direction == "W"
-	@direction = "S"
-elsif @direction == "S"
-	@direction = "E"
-elsif @direction == "E"
-	@direction = "N"
+	def initialize (x,y,direction)
+		@x=x
+		@y=y
+		@direction=direction
 
 	end
 
-end
+	def read_instruction (instruction)
+		if instruction == "M"
+			move
+		elsif instruction == "L"
+			turn_left
+		elsif instruction == "R"
+			turn_right
+		else
+			puts "Invalid input"
 
-def turn_right
+		end
+	end
+	def turn_left
 
-	if @direction == "N"
-		@direction = "E"
+		if @direction == "N"
+			@direction = "W"
+		elsif @direction == "W"
+			@direction = "S"
+		elsif @direction == "S"
+			@direction = "E"
+		elsif @direction == "E"
+			@direction = "N"
+
+		end
+
+	end
+
+	def turn_right
+
+		if @direction == "N"
+			@direction = "E"
 		elsif @direction == "W"
 			@direction = "N"
 		elsif @direction == "S"
 			@direction = "W"
 		elsif @direction == "E"
 			@direction = "S"
-	end
-
-end
-
-
-def move
-
-if @direction == "N"
-		@y+=1
-	elsif @direction == "W"
-		@x-=1
-	elsif @direction == "E"
-		@x+=1
-	elsif @direction == "S"
-		@y-=1	
+		end
 
 	end
-end
+
+
+	def move
+
+		if @direction == "N"
+			@y+=1
+		elsif @direction == "W"
+			@x-=1
+		elsif @direction == "E"
+			@x+=1
+		elsif @direction == "S"
+			@y-=1	
+
+		end
+	end
 
 end
 
@@ -71,6 +72,8 @@ rover1 = Rover.new(0,0,"N")
 puts "Would you like to move L, R, or M?"
 user_input = gets.chomp
 
-rover1.read_instruction (user_input)
+rover1.read_instruction (user_input.upcase)
 
 puts "Rover's current position is #{rover1.x}, #{rover1.y}, facing #{rover1.direction}"
+
+puts "Rover's x poistion is #{rover1.x} y is #{rover1.y}"
